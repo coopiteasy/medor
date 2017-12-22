e587a9f697338619d5d8307c878484f43b51d52a
Rémy Taymans <remytaymans@gmail.com>
2017-12-22 10:18:39 +0100

    [ADD] website_theme: initiate repository

---
+++ medor_website_theme/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Medor Website Theme',
+
+    'summary': """
+        Apply Medor design rules.
+    """,
+    'description': """
+    """,
+
+    'author': 'OSP',
+    'website': "https://github.com/coopiteasy/medor",
+
+    'license': "AGPL-3",
+    'category': 'Themes',
+    'version': '9.0.0.1',
+
+    'depends': ['website'],
+
+    'data': [
+        'views/assets.xml',
+    ]
+}
