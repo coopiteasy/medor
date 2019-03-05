6cfec52b825789524c5e3121ff730b16db9f3a2b
Rémy Taymans <remytaymans@gmail.com>
2019-03-05 16:37:05 +0100

    [ADD] medor_website_product_subscription: Gift link
    Add a link to the gift voucher, when a person subscribe to a new subscription.

---
+++ medor_website_product_subscription/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Coop IT Easy SCRLfs
+#     Rémy Taymans <remy@coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Medor Website Product Subscription',
+
+    'summary': """
+        Add Medor specific change to Website Product Subscription.
+    """,
+    'description': """
+    """,
+
+    'author': 'Coop IT Easy SCRLfs',
+    'website': "https://coopiteasy.be",
+
+    'license': "AGPL-3",
+    'category': "Website",
+    'version': '9.0.1.0',
+
+    'depends': ['website_product_subscription'],
+
+    'data': [
+        'views/website_product_subscription_templates.xml',
+    ]
+}
