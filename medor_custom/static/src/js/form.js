87167bf49f1b9443b0aa0df3979490718b9dd4f6
Rémy Taymans <remytaymans@gmail.com>
2019-09-04 19:55:36 +0200

    [ADD] medor_custom: Selection of delivery method in subscription form

--- medor_custom/static/src/js/form.js
+++ medor_custom/static/src/js/form.js
@@ -8,32 +8,36 @@ odoo.define('medor_custom.oe_user_form', function (require) {
         "use strict";
         var ajax = require('web.ajax');
 
-        $(".oe_delivery_form").each(function () {
+        function hide_friend_address() {
+            var deliv_method = $("input[name='delivery_method']:checked").val();
 
-            function hide_friend_address() {
-                var deliv_method = $("input[name='delivery_method']:checked").val();
-
-                if(deliv_method == "friend") {
-                    $(".title-friend").show('quick');
-                    $(".field-friend_name").show('quick');
-                    $(".field-friend_street").show('quick');
-                    $(".field-friend_zip_code").show('quick');
-                    $(".field-friend_city").show('quick');
-                    $(".field-friend_country_id").show('quick');
-                } else {
-                    $(".title-friend").hide('quick');
-                    $(".field-friend_name").hide('quick');
-                    $(".field-friend_street").hide('quick');
-                    $(".field-friend_zip_code").hide('quick');
-                    $(".field-friend_city").hide('quick');
-                    $(".field-friend_country_id").hide('quick');
-                }
+            if(deliv_method == "friend") {
+                $(".title-friend").show('quick');
+                $(".field-friend_name").show('quick');
+                $(".field-friend_street").show('quick');
+                $(".field-friend_zip_code").show('quick');
+                $(".field-friend_city").show('quick');
+                $(".field-friend_country_id").show('quick');
+            } else {
+                $(".title-friend").hide('quick');
+                $(".field-friend_name").hide('quick');
+                $(".field-friend_street").hide('quick');
+                $(".field-friend_zip_code").hide('quick');
+                $(".field-friend_city").hide('quick');
+                $(".field-friend_country_id").hide('quick');
             }
-            hide_friend_address();
+        }
 
-            $("input[name='delivery_method']").click(function(ev) {
-                hide_friend_address();
-            });
+        $("input[name='delivery_method']").click(function(ev) {
+            hide_friend_address();
+        });
+
+        $(".oe_delivery_form").each(function () {
+            hide_friend_address();
+        });
+
+        $(".oe_delivery_method_info").each(function () {
+            hide_friend_address();
         });
     });
 });
