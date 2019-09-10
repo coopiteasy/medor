dda6130a2a7cc67e9a9dda0ec27f2a7442406898
Rémy Taymans <remytaymans@gmail.com>
2019-09-10 17:31:32 +0200

    [REF] medor_custom: Selection of delivery method in subscribe form

--- medor_custom/controllers/__init__.py
+++ medor_custom/controllers/__init__.py
@@ -2,3 +2,4 @@ from . import trial_subscription
 from . import delivery_form
 from . import delivery
 from . import product_subscription
+from . import subscribe
--- medor_custom/static/src/js/form.js
+++ medor_custom/static/src/js/form.js
@@ -8,36 +8,48 @@ odoo.define('medor_custom.oe_user_form', function (require) {
         "use strict";
         var ajax = require('web.ajax');
 
-        function hide_friend_address() {
+        function toggle_friend_address_fields() {
             var deliv_method = $("input[name='delivery_method']:checked").val();
 
             if(deliv_method == "friend") {
                 $(".title-friend").show('quick');
                 $(".field-friend_name").show('quick');
+                $("input[name='friend_name']").prop('required', true);
                 $(".field-friend_street").show('quick');
+                $("input[name='friend_street']").prop('required', true);
                 $(".field-friend_zip_code").show('quick');
+                $("input[name='friend_zip_code']").prop('required', true);
                 $(".field-friend_city").show('quick');
+                $("input[name='friend_city']").prop('required', true);
                 $(".field-friend_country_id").show('quick');
             } else {
                 $(".title-friend").hide('quick');
                 $(".field-friend_name").hide('quick');
+                $("input[name='friend_name']").prop('required', false);
                 $(".field-friend_street").hide('quick');
+                $("input[name='friend_street']").prop('required', false);
                 $(".field-friend_zip_code").hide('quick');
+                $("input[name='friend_zip_code']").prop('required', false);
                 $(".field-friend_city").hide('quick');
+                $("input[name='friend_city']").prop('required', false);
                 $(".field-friend_country_id").hide('quick');
             }
         }
 
         $("input[name='delivery_method']").click(function(ev) {
-            hide_friend_address();
+            toggle_friend_address_fields();
         });
 
         $(".oe_delivery_form").each(function () {
-            hide_friend_address();
+            toggle_friend_address_fields();
+        });
+
+        $(".oe_subscribe_form").each(function () {
+            toggle_friend_address_fields();
         });
 
         $(".oe_delivery_method_info").each(function () {
-            hide_friend_address();
+            toggle_friend_address_fields();
         });
     });
 });
