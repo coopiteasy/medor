7afafa851fd6f2c60e52873ccfc5c1868fbcff57
Rémy Taymans <remytaymans@gmail.com>
2019-09-12 17:29:46 +0200

    [FIX] medor_w_p_subscription: Required field in edit user form

--- medor_website_product_subscription/static/src/js/form.js
+++ medor_website_product_subscription/static/src/js/form.js
@@ -3,30 +3,38 @@ odoo.define('medor_website_product_subscription.oe_user_form', function (require
         "use strict";
         var ajax = require('web.ajax');
 
-        $(".oe_user_form").each(function () {
+        function toggle_invoice_address_fields() {
+            var inv_add = $("input[name='invoice_address']").is(":checked");
 
-            function hide_invoice_address() {
-                var inv_add = $("input[name='invoice_address']:checked").val();
-
-                if(inv_add == "on") {
-                    $(".title-inv_address").show('quick');
-                    $(".field-inv_street").show('quick');
-                    $(".field-inv_zip_code").show('quick');
-                    $(".field-inv_city").show('quick');
-                    $(".field-inv_country_id").show('quick');
-                } else {
-                    $(".title-inv_address").hide('quick');
-                    $(".field-inv_street").hide('quick');
-                    $(".field-inv_zip_code").hide('quick');
-                    $(".field-inv_city").hide('quick');
-                    $(".field-inv_country_id").hide('quick');
-                }
+            if(inv_add) {
+                $(".title-inv_address").show('quick');
+                $(".field-inv_street").show('quick');
+                $("input[name='inv_street']").prop('required', true);
+                $(".field-inv_zip_code").show('quick');
+                $("input[name='inv_zip_code']").prop('required', true);
+                $(".field-inv_city").show('quick');
+                $("input[name='inv_city']").prop('required', true);
+                $(".field-inv_country_id").show('quick');
+                $("input[name='inv_country_id']").prop('required', true);
+            } else {
+                $(".title-inv_address").hide('quick');
+                $(".field-inv_street").hide('quick');
+                $("input[name='inv_street']").prop('required', false);
+                $(".field-inv_zip_code").hide('quick');
+                $("input[name='inv_zip_code']").prop('required', false);
+                $(".field-inv_city").hide('quick');
+                $("input[name='inv_city']").prop('required', false);
+                $(".field-inv_country_id").hide('quick');
+                $("input[name='inv_country_id']").prop('required', false);
             }
-            hide_invoice_address();
+        }
 
-            $("input[name='invoice_address']").click(function(ev) {
-                hide_invoice_address();
-            });
+        $("input[name='invoice_address']").click(function(ev) {
+            toggle_invoice_address_fields();
+        });
+
+        $(".oe_user_form").each(function () {
+            toggle_invoice_address_fields();
         });
     });
 });
