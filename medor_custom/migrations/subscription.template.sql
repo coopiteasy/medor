84f4ae3707ab0ca333ffe03bf5222b785a3530a8
robin.keunen <robin@keunen.net>
2019-09-19 11:21:02 +0200

    [ADD] set start date to last request payment date - sql query

--- medor_custom/migrations/subscription.template.sql
+++ medor_custom/migrations/subscription.template.sql
@@ -53,3 +53,24 @@ from (
      ) data_table
 where id = subscription_id
 ;
+
+-- update start date for historical users
+update product_subscription_object
+set start_date = data_table.payment_date
+from (
+    select distinct on (pso.subscriber, pso.id)
+           pso.subscriber as subscriber_id,
+           pso.id as subscription_id,
+           pso.name as subscription_name,
+           psr.name,
+           pso.start_date,
+           psr.payment_date
+    from product_subscription_object pso
+    join product_subscription_request psr on pso.subscriber = psr.subscriber
+    where pso.state in ('ongoing', 'renew')
+       and psr.state = 'paid'
+--        and pso.subscriber = 7525
+    order by pso.subscriber, pso.id, psr.payment_date desc
+     ) data_table
+where id = subscription_id
+  and product_subscription_object.state = 'ongoing'
