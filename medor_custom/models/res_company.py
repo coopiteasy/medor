db931f53eb302162ccee146633285a83564bf05b
Robin Keunen <robin@keunen.net>
2023-11-16 13:46:18 +0100

    [ADD] medor_custom: nb web subscribers

--- medor_custom/models/res_company.py
+++ medor_custom/models/res_company.py
@@ -19,34 +19,48 @@ class ResCompany(models.Model):
     nb_trial_subscribers = fields.Integer(
         string="Nb Trial Subscribers", compute="_compute_company_data"
     )
+    nb_web_subscribers = fields.Integer(
+        string="Nb Web Subscribers", compute="_compute_company_data"
+    )
 
     @api.multi
     def _compute_company_data(self):
         for company in self:
-            subscriptions = (
-                self.env["product.subscription.object"]
-                .sudo()
-                .search(
-                    [
-                        ("state", "in", ["ongoing", "renew"]),
-                    ]
-                )
-            )
-
-            subscriber_ids = (
-                subscriptions.filtered(lambda s: not s.is_trial)
-                .mapped("subscriber")
-                .ids
-            )
-            company.nb_subscribers = len(set(subscriber_ids))
-
-            trial_subscriber_ids = (
-                subscriptions.filtered(lambda s: s.is_trial).mapped("subscriber").ids
-            )
-            company.nb_trial_subscribers = len(set(trial_subscriber_ids))
-
-            cooperators = self.env["res.partner"].sudo().search([("member", "=", True)])
-            company.nb_cooperators = len(cooperators)
+            query = """
+            select 'nb_cooperators' as metric, count(id)
+            from res_partner rp
+            where rp.member and active
+            union
+            select 'nb_subscribers' as metric, count(distinct subscriber)
+            from product_subscription_object pso
+                     join product_subscription_template pst ON pst.id = pso.template
+            where not pso.is_trial
+              and pso.state in ('ongoing', 'renew')
+            union
+            select 'nb_trial_subscribers' as metric, count(distinct subscriber)
+            from product_subscription_object pso
+                     join product_subscription_template pst ON pst.id = pso.template
+            where pso.is_trial
+              and pso.state in ('ongoing', 'renew')
+            union
+            select 'nb_web_subscribers' as metric, count(distinct pso.subscriber)
+            from product_subscription_object pso
+                     join product_subscription_template pst ON pst.id = pso.template
+                     join res_partner rp on rp.id = pso.subscriber
+            where not pso.is_trial and pst.is_web_subscription
+              and pso.state in ('ongoing', 'renew', 'terminated')
+              and pso.end_date >= now()
+              and rp.active;
+            """
+            self.env.cr.execute(query)
+            res = self.env.cr.fetchall()
+            metrics = {}
+            for k, v in res:
+                metrics[str(k)] = int(v)
+            company.nb_cooperators = metrics["nb_cooperators"]
+            company.nb_subscribers = metrics["nb_subscribers"]
+            company.nb_trial_subscribers = metrics["nb_trial_subscribers"]
+            company.nb_web_subscribers = metrics["nb_web_subscribers"]
 
     @api.model
     def get_deposit_point(self):
